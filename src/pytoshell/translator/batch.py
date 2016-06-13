be653ead490cab8ab263ecf524ac30b5dcb1962f
Hong-She Liang <starofrainnight@gmail.com>
2016-06-13 20:29:40 +0800

    Completed basic variants process

--- src/pytoshell/translator/batch.py
+++ src/pytoshell/translator/batch.py
@@ -2,13 +2,140 @@ import ast
 from . import base
 from .. import _get_data_path
 
-class Translator(base.Translator):
-    def generic_visit(self, node):
-        print(type(node).__name__)
-        ast.NodeVisitor.generic_visit(self, node)
+class Source(object):
+    def __init__(self):
+        self.front = []
+        self.back = []
 
-    def generate(self, node):
-        self.visit(node)
-        # print(node)
+    def append(self, other_source):
+        self.front += other_source.front
+        self.back = other_source.back + self.back
+
+class Stack(list):
+    def push(self, value):
+        self.append(value)
+
+    @property
+    def top(self):
+        return self[len(self) - 1]
+
+class Translator(base.Translator):
+    def __init__(self):
+        self._stack = Stack()
+        self._object_id = 0
+
+    def _new_object_id(self):
+        self._object_id += 1
+        return self._object_id
+
+    def _get_object_name(self, node):
+        if node.id not in self._stack.top:
+            self._stack.top[node.id] = self._new_object_id()
+
+        return "__PTSO%s" % self._stack.top[node.id]
+
+    def _get_temp_object_name(self):
+        return "__PTSTMPO%s" % self._new_object_id()
+
+    def _gen_set_env(self, name, value="", do_math=False):
+        opt = ""
+        if do_math:
+            opt = "/a"
+
+        return 'set %s "%s=%s"' % (opt, name, value)
+
+    def _parse_env(self, name, value="", do_math=False):
+        source = Source()
+        if not isinstance(name, str):
+            name = self._get_object_name(name)
+        source.front.append(self._gen_set_env(name, value, do_math=do_math))
+        source.back.insert(0, self._gen_set_env(name))
+        return source
+
+    def _parse_value(self, value):
+        source = Source()
+        variant_name = self._get_temp_object_name()
+
+        if type(value) == ast.Num:
+            # Use value directly
+            variant_name = value.n
+        elif type(value) == ast.Str:
+            # Use value directly
+            variant_name = value.s
+        elif type(value) == ast.Call:
+            print("Not implemented call value!")
+        elif type(value) == ast.BinOp:
+            left_source, left_variant = self._parse_value(value.left)
+            right_source, right_variant = self._parse_value(value.right)
+
+            operators = {
+                ast.Add:"+",
+                ast.Sub:"-",
+                ast.Mult:"*",
+                ast.Div:"/",
+                ast.Mod:"%",
+                ast.LShift:"<<",
+                ast.RShift:">>",
+                ast.BitOr:"|",
+                ast.BitAnd:"&",
+                ast.BitXor:"^",
+            }
+
+            opt = operators[type(value.op)]
+
+            source.append(left_source)
+            source.append(right_source)
+            source.append(self._parse_env(
+                variant_name,
+                "%s%s%s" % (left_variant, opt, right_variant),
+                do_math=True))
+
+        return source, variant_name
+
+    def _parse_assign(self, name, value):
+        source = Source()
+
+        sub_source, variant_name = self._parse_value(value)
+
+        source.front += sub_source.front
+        source.append(self._parse_env(name, variant_name, do_math=True))
+        source.front += sub_source.back
+
+        return source
+
+    def _parse_node(self, node):
+        source = Source()
+
+        if type(node) == ast.Assign:
+            atarget = node.targets[0]
+            if type(atarget) == ast.Name:
+                sub_source = self._parse_assign(atarget, node.value)
+                source.append(sub_source)
+            elif type(atarget) == ast.Tuple:
+                for i in range(len(atarget.elts)):
+                    avariable = atarget.elts[i]
+                    value = node.value.elts[i]
+                    sub_source = self._parse_assign(avariable, value)
+                    source.append(sub_source)
+
+        if "body" in node.__dict__:
+            self._stack.push({})
+            for sub_node in node.body:
+                sub_source = self._parse_node(sub_node)
+                source.append(sub_source)
+            self._stack.pop()
+
+        return source
+
+    def translate(self, node):
         print(ast.dump(node))
-        return ""
+
+        self._stack.push({})
+        source = self._parse_node(node)
+        lines = []
+        lines += source.front
+        lines += source.back
+
+        print(lines)
+
+        return "\n".join(lines)
