e07bd73880d48c245e115a9a45ac42925bb5e694
Hong-She Liang <starofrainnight@gmail.com>
2016-06-16 21:17:40 +0800

    OK, we have a basic site library

--- src/pytoshell/data/batch/site.bat
+++ src/pytoshell/data/batch/site.bat
@@ -1,6 +1,15 @@
-:@PYTSVTYPE
+:: The basic library included all routines that needs by pytoshell
+
+:: Get the type of an object
+:@PYTSVTYPE OBJECT
 SETLOCAL
-ECHO %1
-SET "@PYTSR=ABC"
+CALL :@PYTSVSTR_REMOVE_QUOTES %1
+SET "@PYTSV-OBJECT=%@PYTSR%"
+FOR /F "tokens=1 delims=@" %%A IN ("%@PYTSV-OBJECT%") DO SET "@PYTSR=%%A"
+ENDLOCAL & SET "@PYTSR=%@PYTSR%"
+EXIT /B %ERRORLEVEL%
+
+:@PYTSVSTR_REMOVE_QUOTES SELF
+FOR /F "useback tokens=*" %%A IN ('%1') DO SET "@PYTSR=%%~A"
 ENDLOCAL & SET "@PYTSR=%@PYTSR%"
 EXIT /B %ERRORLEVEL%
