62be1189182dea54dc7be0e109bcbbb21fc99194
Hong-She Liang <starofrainnight@gmail.com>
2016-06-23 15:02:55 +0800

    Original first argument of _parse_value() must be node

--- src/pytoshell/translator/batch.py
+++ src/pytoshell/translator/batch.py
@@ -379,22 +379,21 @@ class Translator(base.Translator):
 
         return source
 
-    def _parse_value(self, value, variant=None):
+    def _parse_value(self, node, variant=None):
         source = Source(self._cg)
 
-        node = value
         if variant is None:
             variant = self._ret_variant
 
-        if isinstance(value, ast.Num):
-            source.add_initialize(self._cg.set_variant(variant, value.n, "int"))
-        elif isinstance(value, ast.Str):
-            source.add_initialize(self._cg.set_variant(variant, value.s, "str"))
-        elif isinstance(value, ast.Name):
-            source.add_initialize(self._cg.set_variant(variant, Variant(value.id)))
-        elif isinstance(value, ast.Tuple):
+        if isinstance(node, ast.Num):
+            source.add_initialize(self._cg.set_variant(variant, node.n, "int"))
+        elif isinstance(node, ast.Str):
+            source.add_initialize(self._cg.set_variant(variant, node.s, "str"))
+        elif isinstance(node, ast.Name):
+            source.add_initialize(self._cg.set_variant(variant, Variant(node.id)))
+        elif isinstance(node, ast.Tuple):
             elements = []
-            for new_value in value.elts:
+            for new_value in node.elts:
                 temp_variant = source.create_temp_varaint()
                 sub_source = self._parse_value(new_value, temp_variant)
                 source.append(sub_source)
@@ -402,26 +401,26 @@ class Translator(base.Translator):
 
             source.add_initialize(self._cg.set_variant(
                 variant, '"%s"' %" ".join(elements), "tuple"))
-        elif isinstance(value, ast.Call):
-            sub_source = self._gen_call(value)
+        elif isinstance(node, ast.Call):
+            sub_source = self._gen_call(node)
             source.append(sub_source)
-        elif isinstance(value, ast.FunctionDef):
+        elif isinstance(node, ast.FunctionDef):
             source.add_initialize("") # Add a new line before function definition
-            source.add_initialize(Function(value.name).id_)
-            for an_arg in value.args.args:
+            source.add_initialize(Function(node.name).id_)
+            for an_arg in node.args.args:
                 an_arg_variant = Variant(an_arg.arg)
                 source.add_initialize('set "%s=%%%%1%%" & set "%s=%%%%1%%"' % (
                     an_arg_variant.id_, an_arg_variant.type_info.id_))
                 source.add_initialize('shift')
             source.add_finalize(self._cg.raw_return_("%ERRORLEVEL%"))
-        elif isinstance(value, ast.BinOp):
+        elif isinstance(node, ast.BinOp):
 
             left_temp_variant = source.create_temp_varaint()
-            left_source = self._parse_value(value.left, left_temp_variant)
+            left_source = self._parse_value(node.left, left_temp_variant)
             source.append(left_source)
 
             right_temp_variant = source.create_temp_varaint()
-            right_source = self._parse_value(value.right, right_temp_variant)
+            right_source = self._parse_value(node.right, right_temp_variant)
             source.append(right_source)
 
             operators = {
@@ -437,8 +436,8 @@ class Translator(base.Translator):
                 ast.BitXor:["^", "__xor__"],
             }
 
-            opt = operators[type(value.op)][0]
-            opt_name = operators[type(value.op)][1]
+            opt = operators[type(node.op)][0]
+            opt_name = operators[type(node.op)][1]
             afunction = Function("%%%s%%.%s" % (left_temp_variant.type_info.id_.lower(), opt_name))
             source.add_initialize(self._cg.invoke(
                 afunction,
@@ -446,18 +445,18 @@ class Translator(base.Translator):
             ))
             if variant.tag != Object.TAG_RET:
                 source.add_initialize(self._cg.set_variant(variant, self._ret_variant))
-        elif isinstance(value, ast.Return):
-            if value.value is not None:
-                sub_source = self._parse_value(value.value)
+        elif isinstance(node, ast.Return):
+            if node.value is not None:
+                sub_source = self._parse_value(node.value)
                 source.append(sub_source)
             source.add_initialize(self._cg.return_())
-        elif isinstance(value, ast.NameConstant):
+        elif isinstance(node, ast.NameConstant):
             constants_table = {
                 True:1,
                 False:0,
             }
             source.add_initialize(self._cg.set_variant(
-                variant, constants_table[value.value], "bool"))
+                variant, constants_table[node.value], "bool"))
         elif isinstance(node, ast.Assign):
             atarget = node.targets[0]
             with source.start_temp_clearup():
@@ -478,6 +477,8 @@ class Translator(base.Translator):
             with source.start_temp_clearup():
                 sub_source = self._parse_value(node)
                 source.append(sub_source)
+        elif isinstance(node, ast.If):
+            pass
 
         return source
 
